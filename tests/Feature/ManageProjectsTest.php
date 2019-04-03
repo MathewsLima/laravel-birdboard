8a7033f584c9b786d711cfd533a15d94729fae33
Mathews Lima <mathews.limaa@gmail.com>
2019-04-03 10:20:33 -0300

    rename ProjectsTest to ManageProjectsTest - new tests for check if user is guest

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ProjectsTest extends TestCase
+class ManageProjectsTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
@@ -18,13 +18,37 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
-    public function only_authenticated_users_can_create_projects()
+    public function guests_cannot_create_projects()
     {
         $attributes = factory(Project::class)->raw();
 
         $this->post('/projects', $attributes)
             ->assertRedirect('login');
     }
+
+    /** @test */
+    public function guests_may_not_view_projects()
+    {
+        $this->get('/projects')
+            ->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guests_cannot_view_a_single_project()
+    {
+        $project = factory(Project::class)->create();
+
+        $this->get($project->path())
+            ->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guest_cannot_view_create_page()
+    {
+        $this->get('/projects/create')
+            ->assertRedirect('/login');
+    }
+
 
     /** @test */
     public function a_user_can_create_a_project()
@@ -45,13 +69,27 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_view_a_project()
+    public function a_user_can_view_their_project()
     {
-        $project = factory(Project::class)->create(['title' => '']);
+        $this->withoutExceptionHandling();
+
+        $this->login();
+
+        $project = factory(Project::class)->create(['user_id' => auth()->id()]);
 
         $this->get($project->path())
             ->assertSee($project->title)
             ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+        $project = factory(Project::class)->create();
+
+        $this->login()
+            ->get($project->path())
+            ->assertForbidden();
     }
 
     /** @test */
