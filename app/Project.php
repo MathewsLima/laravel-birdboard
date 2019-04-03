7686f7112ce586998064febebc3e356f791c7c9c
Mathews Lima <mathews.limaa@gmail.com>
2019-04-03 12:05:32 -0300

    add tasks relationship, new method for add task

--- app/Project.php
+++ app/Project.php
@@ -13,8 +13,18 @@ class Project extends Model
         return "/projects/{$this->id}";
     }
 
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function addTask(string $body)
+    {
+        return $this->tasks()->create(['body' => $body]);
     }
 }
