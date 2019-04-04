dad2d15a6235be5106ee98ddc8d9963c6d39ed1c
Mathews Lima <mathews.limaa@gmail.com>
2019-04-04 09:01:52 -0300

    create a route for edit project

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::post('projects', 'ProjectController@store');
     Route::get('projects/create', 'ProjectController@create');
     Route::get('projects/{project}', 'ProjectController@show');
+    Route::get('projects/{project}/edit', 'ProjectController@edit');
     Route::patch('projects/{project}', 'ProjectController@update');
 
     Route::post('projects/{project}/tasks', 'ProjectTaskController@store');
