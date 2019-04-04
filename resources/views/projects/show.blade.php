03224b36bcb44ca0821b85a47d142058e56d2e18
Mathews Lima <mathews.limaa@gmail.com>
2019-04-04 08:02:10 -0300

    add textarea for notes field

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -42,7 +42,22 @@
 
                 <div>
                     <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
-                    <textarea class="card w-full" style="min-height: 200px">Lorem Ipsum...</textarea>
+
+                    <form action="{{ $project->path() }}" method="POST">
+                        @method('PATCH')
+                        @csrf
+
+                        <textarea
+                            name="notes"
+                            class="card w-full mb-4"
+                            style="min-height: 200px"
+                            placeholder="Anything special that you want to make a note of?">
+                            {{ $project->notes }}
+                        </textarea>
+
+                        <button class="button">Save</button>
+                    </form>
+
                 </div>
             </div>
 
