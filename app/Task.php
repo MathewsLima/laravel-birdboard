f44d544e9716b4b86d136b09dded3864f0668934
Mathews Lima <mathews.limaa@gmail.com>
2019-04-04 11:13:11 -0300

    add casts property, override boot method - new method for mark task as completed or incomplete

--- app/Task.php
+++ app/Task.php
@@ -7,7 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['body', 'completed', 'project_id'];
-    protected $touches  = ['project'];
+
+    protected $touches = ['project'];
+
+    protected $casts = [
+        'completed' => 'bool'
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($task) {
+            $task->project->recordActivity('created_task');
+        });
+    }
 
     public function project()
     {
@@ -17,5 +31,17 @@ class Task extends Model
     public function path()
     {
         return "{$this->project->path()}/tasks/{$this->id}";
+    }
+
+    public function complete()
+    {
+        $this->update(['completed' => true]);
+
+        $this->project->recordActivity('completed_task');
+    }
+
+    public function incomplete()
+    {
+        $this->update(['completed' => false]);
     }
 }
