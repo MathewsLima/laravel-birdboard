2fd92ad5cd6cd3a2e08cf0ac082443e59a8deaa2
Mathews Lima <mathews.limaa@gmail.com>
2019-04-04 11:13:57 -0300

    change completed to false as default value

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 $factory->define(Task::class, function (Faker $faker) {
     return [
         'body'       => $faker->sentence,
-        'completed'  => true,
+        'completed'  => false,
         'project_id' => function () {
             return factory(Project::class)->create()->id;
         }
