4992730110f100495c2c4a83fa267a08f5e92257
Mathews Lima <mathews.limaa@gmail.com>
2019-04-05 09:13:27 -0300

    create a trait for reusable code between Project and Task. This trait replace the Observers from Project and Task class

---
+++ app/RecordsActivity.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Arr;
+
+trait RecordsActivity
+{
+    public $oldAttributes = [];
+
+    public static function bootRecordsActivity()
+    {
+        $recordableEvents = self::recordableModelEvents();
+
+        $recordableEvents->each(function ($event) {
+            self::triggerEvent($event);
+        });
+    }
+
+    protected static function triggerEvent(string $event)
+    {
+        static::$event(function ($model) use ($event) {
+            $model->recordActivity($model->activityDescription($event));
+        });
+
+        if ($event === 'updated') {
+            static::updating(function ($model) {
+                $model->oldAttributes = $model->getOriginal();
+            });
+        }
+    }
+
+    protected static function recordableModelEvents()
+    {
+        return isset(static::$recordableEvents)
+            ? collect(static::$recordableEvents)
+            : collect(['created', 'updated', 'deleted']);
+    }
+
+    protected function activityDescription(string $description)
+    {
+        return "{$description}_" . strtolower(class_basename($this));
+    }
+
+    public function recordActivity(string $description)
+    {
+        $this->activity()->create([
+            'description' => $description,
+            'changes'     => $this->activityChanges(),
+            'project_id'  => class_basename($this) === 'Project' ? $this->id : $this->project_id
+        ]);
+    }
+
+    public function activity()
+    {
+        return $this->morphMany(Activity::class, 'subject')->latest();
+    }
+
+    public function activityChanges()
+    {
+        if ($this->wasChanged()) {
+            return [
+                'before' => Arr::except(array_diff($this->oldAttributes, $this->getAttributes()), 'updated_at'),
+                'after'  => Arr::except($this->changes, 'updated_at')
+            ];
+        }
+    }
+}
