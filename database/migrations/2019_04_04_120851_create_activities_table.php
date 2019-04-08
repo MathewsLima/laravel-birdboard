d915732c41ff5208b35a8634e90b6e286ae32378
Mathews Lima <mathews.limaa@gmail.com>
2019-04-08 11:03:16 -0300

    add user_id in migration - add user_id in fillable array - add user relationship

--- database/migrations/2019_04_04_120851_create_activities_table.php
+++ database/migrations/2019_04_04_120851_create_activities_table.php
@@ -14,9 +14,11 @@ class CreateActivitiesTable extends Migration
             $table->text('changes')->nullable();
             $table->nullableMorphs('subject');
             $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
 
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
