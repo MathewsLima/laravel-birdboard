a9771dada8cb50b25a2c73ab0b365e15ad66ca21
Mathews Lima <mathews.limaa@gmail.com>
2019-04-08 16:05:29 -0300

    change css, add @can for only owner delete project

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,18 +1,20 @@
-<div class="card" style="height: 200px">
+<div class="card flex flex-col" style="height: 200px">
     <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
         <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
     </h3>
 
-    <div class="text-grey mb-4">
+    <div class="flex-1 text-grey mb-4">
         {{ \Str::limit($project->description, 100) }}
     </div>
 
-    <footer>
-        <form action="{{ $project->path() }}" method="POST" class="text-right">
-            @method('DELETE')
-            @csrf
+    @can('manage', $project)
+        <footer>
+            <form action="{{ $project->path() }}" method="POST" class="text-right">
+                @method('DELETE')
+                @csrf
 
-            <button type="submit" class="text-xs">Delete</button>
-        </form>
-    </footer>
+                <button type="submit" class="text-xs">Delete</button>
+            </form>
+        </footer>
+    @endcan
 </div>
