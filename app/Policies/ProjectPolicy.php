4c489e56fcadcd488aff5a257e951fb49f4087ae
Mathews Lima <mathews.limaa@gmail.com>
2019-04-08 16:02:55 -0300

    new method that check if user is the owner

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -10,8 +10,13 @@ class ProjectPolicy
 {
     use HandlesAuthorization;
 
-    public function update(User $user, Project $project)
+    public function manage(User $user, Project $project)
     {
         return $user->is($project->user);
+    }
+
+    public function update(User $user, Project $project)
+    {
+        return $user->is($project->user) || $project->members->contains($user);
     }
 }
