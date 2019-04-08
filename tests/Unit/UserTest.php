2932d76364c123cbc4c304baa88d01fb11c29bb4
Mathews Lima <mathews.limaa@gmail.com>
2019-04-08 16:01:33 -0300

    new Unit Test for show accessibles user projects

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\User;
+use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -17,5 +18,29 @@ class UserTest extends TestCase
         $user = factory(User::class)->create();
 
         $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+
+    /** @test */
+    public function a_user_has_accessibles_projects()
+    {
+        $john = $this->signIn();
+
+        ProjectFactory::ownedBy($john)
+            ->create();
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        [$sally, $nick] = factory(User::class, 2)->create();
+
+        $project = ProjectFactory::ownedBy($sally)
+            ->create();
+
+        $project->invite($nick);
+
+        $this->assertCount(1, $john->accessibleProjects());
+
+        $project->invite($john);
+
+        $this->assertCount(2, $john->accessibleProjects());
     }
 }
