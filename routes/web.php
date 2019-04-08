780eb55b871f90e44932abd95dd23f2c0e3684b2
Mathews Lima <mathews.limaa@gmail.com>
2019-04-08 11:06:43 -0300

    replace rest route for resource

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,7 @@ Auth::routes();
 Route::redirect('/', '/projects');
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('projects', 'ProjectController@index');
-    Route::post('projects', 'ProjectController@store');
-    Route::get('projects/create', 'ProjectController@create');
-    Route::get('projects/{project}', 'ProjectController@show');
-    Route::get('projects/{project}/edit', 'ProjectController@edit');
-    Route::patch('projects/{project}', 'ProjectController@update');
+    Route::resource('projects', 'ProjectController');
 
     Route::post('projects/{project}/tasks', 'ProjectTaskController@store');
     Route::patch('projects/{project}/tasks/{task}', 'ProjectTaskController@update');
