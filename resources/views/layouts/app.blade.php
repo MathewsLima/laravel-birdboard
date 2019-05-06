47978f8ae1d6640b867cc418839b94bce22cb7ee
Mathews Lima <mathews.limaa@gmail.com>
2019-05-06 16:19:43 -0300

    change bootstrap dropdown for vue component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,20 +50,25 @@
                                 @endif
                             @else
                                 <theme-switcher></theme-switcher>
-                                <a
-                                    class="flex items-center text-default no-underline text-sm"
-                                    href="#" role="button"
-                                    data-toggle="dropdown"
-                                    aria-haspopup="true"
-                                    aria-expanded="false"
-                                    v-pre
-                                >
-                                    <img width="35"
-                                        class="rounded-full mr-3"
-                                        src="{{ gravatar_url(auth()->user()->email) }}">
 
-                                    {{ auth()->user()->name }}
-                                </a>
+                                <dropdown align="right" width="200px">
+                                    <template v-slot:trigger>
+                                        <button class="flex items-center text-default no-underline text-sm focus:outline-none">
+                                            <img width="35"
+                                                class="rounded-full mr-3"
+                                                src="{{ gravatar_url(auth()->user()->email) }}">
+
+                                            {{ auth()->user()->name }}
+                                        </button>
+                                    </template>
+
+                                    <form id="logout-form" action="/logout" method="post">
+                                        @csrf
+
+                                        <button type="submit" class="dropdown-menu-link text-left w-full">Logout</button>
+                                    </form>
+                                </dropdown>
+
                             @endguest
                         </div>
                     </div>
